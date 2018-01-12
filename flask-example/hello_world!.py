86b1ad844baf037440aee48fe322429b1bfa5ec5
Alexanderklau <429095816@qq.com>
2018-01-12 22:38:01 +0800

    fix

---
+++ flask-example/hello_world!.py
@@ -0,0 +1,15 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    user_agent = request.headers.get("User-Agent")
+    return "<p>Your browser is %s</p>" % user_agent
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s</h1>' %name
+
+if __name__ == '__main__':
+    app.run(debug=True)
